f7059f72748d79687256d7dc79b7a0cca2c7a37f
prerak126 <prerak126@gmail.com>
2018-08-12 19:22:59 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './todoList';
+import TodoStore from './todoStore';
+
 
 class App extends Component {
   render() {
@@ -10,9 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TodoList store={TodoStore} />
       </div>
     );
   }
